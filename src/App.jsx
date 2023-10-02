b54dcb41e86daf2e69c62f8ee091177087d1de0c
HabibaSabrina <habibasabrina92@gmail.com>
2023-10-02 16:21:00 +0600

    search field icons added

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <img className='w-[276px]' src="/googlepng.png" alt="" />
-     <div className='relative'>
+    <div className='w-[582px]' >
+      <img className='w-[276px] mx-auto mb-7' src="/googlepng.png" alt="" />
+     <div className='relative '>
      <FaSearch className='text-[#aaaaaa] text-sm top-4 left-5 absolute'></FaSearch>
-     <input className='border border-[#E3E3E3] w-[582px] h-[44px] rounded-full focus:outline-none pl-12' type="text" placeholder='Search Google or type a URL'/>
+     <input className='border border-[#E3E3E3] w-full h-[44px] rounded-full focus:outline-none pl-12' type="text" placeholder='Search Google or type a URL'/>
+     <div className='flex items-center gap-5 absolute top-4 right-6'>
+      <img className='w-3' src="/voice.png" alt="" />
+      <img className='w-3' src="/image.png" alt="" />
+     </div>
      </div>
     </div>
   )
